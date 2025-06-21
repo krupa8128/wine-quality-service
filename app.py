fe497113023d13ccd462b10d52f3c935d1787439
Krupa Patel <krupap0507@gmail.com>
2025-06-21 12:42:27 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+model = joblib.load("wine_model.pkl")
+
+class WineFeatures(BaseModel):
+    fixed_acidity: float
+    volatile_acidity: float
+    citric_acid: float
+    residual_sugar: float
+    chlorides: float
+    free_sulfur_dioxide: float
+    total_sulfur_dioxide: float
+    density: float
+    pH: float
+    sulphates: float
+    alcohol: float
+
+@app.post("/predict")
+def predict(features: WineFeatures):
+    data = [[
+        features.fixed_acidity,
+        features.volatile_acidity,
+        features.citric_acid,
+        features.residual_sugar,
+        features.chlorides,
+        features.free_sulfur_dioxide,
+        features.total_sulfur_dioxide,
+        features.density,
+        features.pH,
+        features.sulphates,
+        features.alcohol
+    ]]
+    prediction = model.predict(data)[0]
+    return {"predicted_quality": int(prediction)}
